c070caac599272934b83d9ba0abd333fe0fea737
leosurwiki <396568314@qq.com>
2016-02-17 22:48:17 -0800

    Add mobile gravity detection

---
+++ 2dnewdemo/Assets/GravityControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Debug.Log(Input.acceleration.x);
+        Debug.Log(Input.acceleration.y);
+  
+	}
+}
---
+++ 2dnewdemo/Assets/LiquidPhysics2D/Prefabs/Pcgravitycontrol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pcgravitycontrol : MonoBehaviour {
+    [Tooltip("The world with that gravity")]
+    public LPManager world;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        LPAPIWorld.SetWorldGravity(world.GetPtr(), Input.acceleration.x*9,Input.acceleration.y*9);
+	}
+}
